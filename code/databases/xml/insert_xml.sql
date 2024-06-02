4a42d2932d3b181ca0374dd1fc164bfe93e1afcd
Gustavo H. K. Oliveira <123880096+gustavo-h-k-oliveira@users.noreply.github.com>
2024-06-02 17:31:51 +0000

    Adicionando script de dados XML

---
+++ code/databases/xml/insert_xml.sql
@@ -0,0 +1,16 @@
+-- Inserir dados XML na tabela ExemploXML
+INSERT INTO ExemploXML (Id, DadosXML)
+VALUES (1, '<cliente><nome>João</nome><idade>30</idade></cliente>');
+
+INSERT INTO ExemploXML (Id, DadosXML)
+VALUES (2, '<cliente><nome>Maria</nome><idade>25</idade></cliente>');
+
+INSERT INTO ExemploXML (Id, DadosXML)
+VALUES (3, '<cliente><nome>Carlos</nome><idade>40</idade></cliente>');
+
+-- Inserir dados XML na tabela ExemploXML com parâmetros seguros
+DECLARE @XmlData XML = '<cliente><nome>João</nome><idade>30</idade></cliente>';
+
+INSERT INTO ExemploXML (Id, DadosXML)
+VALUES (1, @XmlData);
+
